cf234d8d8caf366dd30c0e86a3504936a6dc4bb2
Steven Brown <sgbrown@mac.com>
2017-12-03 03:45:51 +1300

    day 01 a

---
+++ day01b.rb
@@ -0,0 +1,12 @@
+def day01(input)
+
+  input = input + input[0]
+  output = 0
+  for i in 0..input.length-2
+    output += input[i].to_i if input[i] == input[i+1]
+  end
+  return output
+end
+
+input = File.read("day01_input.txt")
+puts day01(input)
---
+++ day01b_spec.rb
@@ -0,0 +1,20 @@
+require "rspec"
+require_relative "day01"
+
+describe "day01" do
+  it "returns 3 for '1122'" do
+    expect(day01("1122")).to eq 3
+  end
+
+  it "returns 4 for '1111'" do
+    expect(day01("1111")).to eq 4
+  end
+
+  it "returns 0 for '1234'" do
+    expect(day01("1234")).to eq 0
+  end
+
+  it "returns 9 for '91212129'" do
+    expect(day01("91212129")).to eq 9
+  end
+end
