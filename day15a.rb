7786c5cc76fd6735426fd3d3905b7aaa71f1b5a8
Steven Brown <sgbrown@mac.com>
2017-12-20 11:12:42 +1300

    solution for Day 15 part One

--- day15a.rb
+++ day15a.rb
@@ -1,12 +1,19 @@
 def day15(input)
-  generator_a =
-  generator_b =
+  input_lines = input.split("\n")
+  generator_a = input_lines[0].split(' ')[-1].to_i
+  generator_b = input_lines[1].split(' ')[-1].to_i
   count = 0
   40_000_000.times do
-
+    generator_a *= 16807
+    generator_b *= 48271
+    generator_a %= 2147483647
+    generator_b %= 2147483647
+    if generator_a % 65536 == generator_b % 65536 then
+      count += 1
+    end
   end
   return count
 end
 
-# input = File.read("day15_input.txt").chomp
-# puts day15(input)
+input = File.read("day15_input.txt").chomp
+puts day15(input)
