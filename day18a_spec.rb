92def9af5b211320df45e960c1f3300abac5205a
Steven Brown <sgbrown@mac.com>
2017-12-23 11:49:14 +1300

    Day 18 part One tidy up test for part Two

--- day18a_spec.rb
+++ day18a_spec.rb
@@ -2,8 +2,8 @@ require "rspec"
 require_relative "day18a"
 
 describe "day18" do
-  it "returns 4 for 'day18_test1.txt' " do
-    input = File.read("day18_test1.txt").chomp
+  it "returns 4 for 'day18a_test1.txt' " do
+    input = File.read("day18a_test1.txt").chomp
     expect(day18(input)).to eq 4
   end
 end
