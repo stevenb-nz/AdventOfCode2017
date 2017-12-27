4bfd2a71f69ef563b13ed644f3cc821055281d71
Steven Brown <sgbrown@mac.com>
2017-12-28 06:57:49 +1300

    solution for Day 20, part One

--- day20a.rb
+++ day20a.rb
@@ -1,7 +1,18 @@
 def day20(input)
-
-return 0
+lines = input.split("\n")
+values = []
+lines.each do |line|
+  line.delete!("p")
+  line.delete!("v")
+  line.delete!("a")
+  line.delete!("=")
+  line.delete!("<")
+  line.delete!(">")
+  value = line.split(',').map(&:to_i)
+  values.push value[6].abs+value[7].abs+value[8].abs
+end
+return values.index(values.min)
 end
 
-# input = File.read("day20_input.txt").chomp
-# puts day20(input)
+input = File.read("day20_input.txt").chomp
+puts day20(input)
