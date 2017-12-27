5a05ae8f909902808301bb9211748de24eff1fab
Steven Brown <sgbrown@mac.com>
2017-12-28 07:03:45 +1300

    Day 20, part One update

--- day20a_spec.rb
+++ day20a_spec.rb
@@ -2,8 +2,8 @@ require "rspec"
 require_relative "day20a"
 
 describe "day20" do
-  it "returns 0 for 'day20_test1.txt' " do
-    input = File.read("day20_test1.txt").chomp
+  it "returns 0 for 'day20a_test1.txt' " do
+    input = File.read("day20a_test1.txt").chomp
     expect(day20(input)).to eq 0
   end
 end
