3a163d2a0963b3d4be82d38af9536d7414d35845
Steven Brown <sgbrown@mac.com>
2017-12-27 22:09:11 +1300

    Day 20 setup

---
+++ day20a.rb
@@ -0,0 +1,7 @@
+def day20(input)
+
+return 0
+end
+
+# input = File.read("day20_input.txt").chomp
+# puts day20(input)
---
+++ day20a_spec.rb
@@ -0,0 +1,9 @@
+require "rspec"
+require_relative "day20a"
+
+describe "day20" do
+  it "returns 0 for 'day20_test1.txt' " do
+    input = File.read("day20_test1.txt").chomp
+    expect(day20(input)).to eq 0
+  end
+end
