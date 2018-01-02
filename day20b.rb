cc83e5970cafc80644baea485269991b536f94dc
Steven Brown <sgbrown@mac.com>
2018-01-03 00:19:26 +1300

    day 20, part 2 progress

--- day20b.rb
+++ day20b.rb
@@ -11,9 +11,20 @@ lines.each do |line|
   particle = line.split(',').map(&:to_i)
   particles.push particle
 end
-  particles.each do |particle|
-
+begin
+  (0...particles.count-1).each do |p1|
+    (p1+1..particles.count-1).each do |p2|
+      if (particles[p1][0]-particles[p2][0]).abs + (particles[p1][1]-particles[p2][1]).abs + (particles[p1][2]-particles[p2][2]).abs == 0 then
+        # collision
+      end
+    end
   end
+  particles.each do |particle|
+    # remove if collided
+    # otherwise, increment position
+  end
+  # print particles count
+end
 return
 end
 
