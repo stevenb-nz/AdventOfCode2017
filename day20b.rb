cbc59d158afb8db873b8822419d701f03adb7b1a
Steven Brown <sgbrown@mac.com>
2018-01-05 22:21:42 +1300

    solution for Day 20, part Two

--- day20b.rb
+++ day20b.rb
@@ -13,7 +13,10 @@ lines.each do |line|
   particle = line.split(',').map(&:to_i)
   particles.push particle
 end
+reps = 0
 begin
+  reps += 1
+  count1 = particles.count
   collided = Set.new
   (0...particles.count-1).each do |p1|
     (p1+1..particles.count-1).each do |p2|
@@ -26,14 +29,22 @@ begin
   temp_particles = []
   particles.each_with_index do |particle,index|
     if !collided.member? index
-      # increment particle
+      particle[5] += particle[8]
+      particle[4] += particle[7]
+      particle[3] += particle[6]
+      particle[2] += particle[5]
+      particle[1] += particle[4]
+      particle[0] += particle[3]
       temp_particles.push particle
     end
   end
   particles = temp_particles
-end #until particles.count > 1
+  if particles.count < count1
+    count1 = particles.count
+  end
+end until reps == 50
 return particles.count
 end
 
-# input = File.read("day20_input.txt").chomp
-# puts day20(input)
+input = File.read("day20_input.txt").chomp
+puts day20(input)
